3f4a4433d7e972467126bfa4136537941896bb7e
dicky ben wijaya <brainsinting@gmail.com>
2020-07-21 21:55:50 +0700

    baru

--- kuliah/pertemuan10/latihan3.php
+++ kuliah/pertemuan10/latihan3.php
@@ -9,6 +9,14 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <!-- Google Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+  <!-- Bootstrap core CSS -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
   <title>Daftar Mahasiswa</title>
 </head>
 
@@ -18,25 +26,37 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
   <a href="">tambah data mahasiswa</a>
   <br><br>
 
-  <table border="1" cellpadding="18" cellspacing="0">
-    <tr>
-      <th>#</th>
-      <th>Gambar</th>
-      <th>Nama</th>
-      <th>Aksi</th>
-    </tr>
-    <?php foreach ($mahasiswa as $m) : ?>
+  <table border="1" cellpadding="18" cellspacing="0" class="table table-hover">
+    <thead class="black white-text">
       <tr>
-        <td>1</td>
-        <td><img src="img/<?= $m['gambar']; ?>" width="60"></td>
-        <td><?= $m['nama']; ?></td>
-        <td>
-          <a href="detail.php?id=<?= $m['id']; ?>">lihat detail</a>
-        </td>
+        <th scope="col">#</th>
+        <th scope="col">Gambar</th>
+        <th scope="col">Nama</th>
+        <th scope="col">Aksi</th>
       </tr>
+    </thead>
+    <?php foreach ($mahasiswa as $m) : ?>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td><img src="img/<?= $m['gambar']; ?>" width="60"></td>
+          <td><?= $m['nama']; ?></td>
+          <td>
+            <a href="detail.php?id=<?= $m['id']; ?>">lihat detail</a>
+          </td>
+        </tr>
+      </tbody>
     <?php endforeach; ?>
   </table>
 
 </body>
 
 </html>
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
--- kuliah/pertemuan10/tambah.php
+++ kuliah/pertemuan10/tambah.php
@@ -20,48 +20,69 @@ if (isset($_POST['tambah'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <!-- Google Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+  <!-- Bootstrap core CSS -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
   <title>form tambah data mahasiswa</title>
 </head>
 
 <body>
   <h3>form tambah data mahasiswa</h3>
   <form action="" method="POST">
-    <ul>
+    <ul style="list-style-type:none;">
       <li>
-        <label>
-          Nama :
-          <input type="text" name="nama" autofocus required>
-        </label>
+        <!-- Material input -->
+        <div class="md-form" style="width: 400px;">
+          <input type="text" id="form1" name="nama" class="form-control" required autofocus autocomplete="off">
+          <label for="form1">Nama</label>
+        </div>
       </li>
       <li>
-        <label>
-          NIM :
-          <input type="text" name="nim" required>
-        </label>
+        <!-- Material input -->
+        <div class="md-form" style="width: 400px;">
+          <input type="text" id="form1" name="nim" class="form-control" required autocomplete="off">
+          <label for="form1">NIM</label>
+        </div>
       </li>
       <li>
-        <label>
-          Jurusan :
-          <input type="text" name="jurusan" required>
-        </label>
+        <!-- Material input -->
+        <div class="md-form" style="width: 400px;">
+          <input type="text" id="form1" name="jurusan" class="form-control" required autocomplete="off">
+          <label for="form1">Jurusan</label>
+        </div>
       </li>
       <li>
-        <label>
-          Kelas :
-          <input type="text" name="kelas" required>
-        </label>
+        <!-- Material input -->
+        <div class="md-form" style="width: 400px;">
+          <input type="text" id="form1" name="kelas" class="form-control" required autocomplete="off">
+          <label for="form1">Kelas</label>
+        </div>
       </li>
       <li>
-        <label>
-          Gambar :
-          <input type="text" name="gambar" required>
-        </label>
+        <!-- Material input -->
+        <div class="md-form" style="width: 400px;">
+          <input type="text" id="form1" name="gambar" class="form-control" required autocomplete="off">
+          <label for="form1">Gambar</label>
+        </div>
       </li>
       <li>
-        <button type="submit" name="tambah">tambah data!</button>
+        <button type="submit" name="tambah" class="btn btn-primary">tambah data!</button>
       </li>
     </ul>
   </form>
 </body>
 
 </html>
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
